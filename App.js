13cf24a6fd0b5b20315a2a5f162c10cd07d889bd
matheusverri <matheus.gvdias2002@hotmail.com>
2022-10-25 21:19:24 -0300

    (Incompleto) Elaborar função de adicionar item e limpar lista

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState("");
+
+  let listItems = [];
+
+  function addItemToListAndClearInput(itemName) {
+    listItems.push(itemName);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text> Matheus Gustavo Verri Dias. R.A: 2020101522</Text>
+      </View>
+      <View style={styles.body}>
+        <TextInput
+          style={styles.input}
+          placeholder="Insira um item"
+          onChangeText={text => setText(text)}
+          value={text}
+        />
+        <Button
+        onPress={addItemToListAndClearInput(text)}
+        title="Adicionar"
+        color="#841584"
+        accessibilityLabel="Adicionar item"
+        />
+        <View>
+          <Text> {listItems} </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +42,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    marginTop: 25,
+  },
+  body: {
+    marginTop: 60,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
